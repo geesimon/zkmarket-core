f95c5d944ec2c1f99fded5fb36f31c9751b14928
geesimon <geesimon@hotmail.com>
2022-08-16 00:33:56 +0800

    Done commitment verification tests

--- test/2_AssetPool.test.js
+++ test/2_AssetPool.test.js
@@ -27,7 +27,6 @@ const bits2PathIndices = (_bitmap, _length) => {
 contract('AssetPool Test', accounts => {
     const ZERO_VALUE = bigInt('890052662763911307850778159620885184910760398780342638619984914703804053834').value; // = keccak256("zkMarket.Finance") % FIELD_SIZE
     const FEE = bigInt(COIN_AMOUNT).shiftRight(2) || bigInt(1e18);
-    const REFUND = bigInt(0);
     const RELAYER = accounts[1];
     const SENDER = accounts[1];
     const OPERATOR = accounts[0];
@@ -52,37 +51,36 @@ contract('AssetPool Test', accounts => {
             nullifier: rbigint(31),
             amount: rbigint(31),
         };
-        // const preimage = Buffer.concat([deposit.nullifier.leInt2Buff(31), deposit.secret.leInt2Buff(31)])
         const preimage = Buffer.concat([
-                                        Buffer.from(bigInt2BytesLE(deposit.nullifier, 31)),
-                                        Buffer.from(bigInt2BytesLE(deposit.secret, 31)),
-                                        Buffer.from(bigInt2BytesLE(deposit.amount, 31))
+                                        Buffer.from(bigInt2BytesLE(transaction.nullifier, 31)),
+                                        Buffer.from(bigInt2BytesLE(transaction.secret, 31)),
+                                        Buffer.from(bigInt2BytesLE(transaction.amount, 31))
                                     ])
 
-        transaction.commitment = hasherFunc(preimage);
+        transaction.commitmentHash = hasherFunc(preimage);
 
         return transaction;
     };
 
-    const packProofData = (proof) => {
+    const packProofData = (_proof) => {
             return [
-                proof.pi_a[0], proof.pi_a[1],
-                proof.pi_b[0][1], proof.pi_b[0][0], proof.pi_b[1][1], proof.pi_b[1][0],
-                proof.pi_c[0], proof.pi_c[1],
+                _proof.pi_a[0], _proof.pi_a[1],
+                _proof.pi_b[0][1], _proof.pi_b[0][0], _proof.pi_b[1][1], _proof.pi_b[1][0],
+                _proof.pi_c[0], _proof.pi_c[1],
             ]
         };
 
-    const verifyMerklePath = (merkleTree, treeLevel, commitment, callLog) => {
-        callLog.event.should.be.equal('InsertCommitment');
-        callLog.args.commitment.should.be.equal(toFixedHex(commitment));
-        callLog.args.root.should.be.equal(toFixedHex(merkleTree.root));
+    const verifyMerklePath = (_merkleTree, _treeLevel, _commitment, _callLog) => {
+        _callLog.event.should.be.equal('InsertCommitment');
+        _callLog.args.commitment.should.be.equal(toFixedHex(_commitment));
+        _callLog.args.root.should.be.equal(toFixedHex(_merkleTree.root));
 
-        const {pathElements, pathIndices} = merkleTree.proof(commitment);
-        const contractPathIndices = bits2PathIndices(Number(callLog.args.pathIndices), treeLevel);
+        const {pathElements, pathIndices} = _merkleTree.proof(_commitment);
+        const contractPathIndices = bits2PathIndices(Number(_callLog.args.pathIndices), _treeLevel);
         // console.log(contractPathIndices, pathIndices)
         pathIndices.join().should.be.equal(contractPathIndices.join());
 
-        callLog.args.pathElements.forEach((n, k) => {
+        _callLog.args.pathElements.forEach((n, k) => {
             let n1 = bigInt(n.slice(2), 16).toString()
             let n2 = bigInt(pathElements[k]).toString();
             // console.log("tree:", n1, "contract:", n2);
@@ -99,55 +97,89 @@ contract('AssetPool Test', accounts => {
             mimcHasher = (left, right) => mimcSponge.F.toObject(mimcSponge.hash(left, right, 0).xL);
 
             paypalUSDCAssetPool = await PaypalUSDCAssetPool.new(
-                                        CommitmentVerifier.address,
-                                        Hasher.address,
-                                        TREE_LEVELS,
-                                        {from: OPERATOR}
-                                        );
-            merkleTree = new MerkleTree(TREE_LEVELS, [], { hashFunction: mimcHasher, zeroElement: ZERO_VALUE});
+                                                                CommitmentVerifier.address,
+                                                                Hasher.address,
+                                                                TREE_LEVELS,
+                                                                {from: OPERATOR}
+                                                                );
+            globalTree = new MerkleTree(TREE_LEVELS, [], { hashFunction: mimcHasher, zeroElement: ZERO_VALUE});
             commitment_verification_key = JSON.parse(fs.readFileSync(CommitmentCircuitVerificationKey));
     })
 
-  describe('#Commitment Verification', () => {
-    it('Should allow paypal relayer register comment', async () => {
-        let commitment = bigInt(42);
-        let amount = bigInt(100);
+    describe('#Commitment Verification', () => {
+        it('Should allow operator register comment if not exists', async () => {
+            let commitment = bigInt(42);
+            let amount = bigInt(100);
+            
+            let isExists = await paypalUSDCAssetPool.commitmentExists(toFixedHex(commitment));
+            isExists.should.equal(false);
+
+            await paypalUSDCAssetPool.registerCommitment(toFixedHex(commitment), amount.toString(), {from: OPERATOR });
+            isExists = await paypalUSDCAssetPool.commitmentExists(toFixedHex(commitment));
+            isExists.should.equal(true);
+
+            await paypalUSDCAssetPool.registerCommitment(toFixedHex(commitment), amount.toString())
+                    .should.be.rejectedWith('The commitment already exists');
+        })
+
+        it('Should only allow contract owner to register commitment', async () => {
+            const commitment = bigInt(33);
+            let amount = bigInt(100);
+    
+            await paypalUSDCAssetPool.registerCommitment(toFixedHex(commitment), amount.toString(), {from: SENDER })
+                .should.be.rejectedWith('Ownable: caller is not the owner');
+        })
+
+        it('Should generte correct proof and detect tampering by SnarkJS', async () => {
+            const transaction = generateTransaction(pedersenHasher);
         
-        let isExists = await paypalUSDCAssetPool.commitmentExists(toFixedHex(commitment));
-        isExists.should.equal(false);
+            const input = { 
+                            commitmentHash: transaction.commitmentHash.toString(),
+                            amount: transaction.amount.toString(),
+                            nullifier: transaction.nullifier.toString(),
+                            secret: transaction.secret.toString()
+                        };
+            // console.log(input)
+    
+            const {proof, publicSignals} = await snarkjs.groth16.fullProve(
+                                                                            input,
+                                                                            CommitmentCircuitWASMFile,
+                                                                            CommitmentCircuitKey
+                                                                            );
+            
+    
+            let result = await snarkjs.groth16.verify(commitment_verification_key, publicSignals, proof);
+            result.should.be.equal(true);
 
-        await paypalUSDCAssetPool.registerCommitment(toFixedHex(commitment), amount.toString());
-        isExists = await paypalUSDCAssetPool.commitmentExists(toFixedHex(commitment));
-        isExists.should.equal(true);
+            // Try to cheat with wrong amount
+            let publicSignalsCopy = JSON.parse(JSON.stringify(publicSignals));
+            publicSignalsCopy[1] = '100'
+            result  = await snarkjs.groth16.verify(commitment_verification_key, publicSignalsCopy, proof);
+            result.should.be.equal(false);
+        })
+
+        it('Should insert commitment if proof is valid', async () => {
+            const transaction = generateTransaction(pedersenHasher);
+        
+            const input = { 
+                            commitmentHash: transaction.commitmentHash.toString(),
+                            amount: transaction.amount.toString(),
+                            nullifier: transaction.nullifier.toString(),
+                            secret: transaction.secret.toString()
+                        };
+            
+            const {proof, publicSignals} = await snarkjs.groth16.fullProve(
+                                                                            input,
+                                                                            CommitmentCircuitWASMFile,
+                                                                            CommitmentCircuitKey
+                                                                            );
+            
+            const proofData = packProofData(proof);
+            
+            await paypalUSDCAssetPool.registerCommitment(toFixedHex(input.commitmentHash), input.amount, {from: OPERATOR });
+            const { logs } = await paypalUSDCAssetPool.proveCommitment(proofData, publicSignals, { from: RELAYER});
+            logs[0].event.should.be.equal('InsertCommitment');
+            logs[0].args.commitment.should.be.equal(toFixedHex(input.commitmentHash));
+        })
     })
-
-    // it('should throw if the commitment already exists', async () => {
-    //     const commitment = bigInt(33);
-    //     let { logs } = await globalHongbao.deposit(toFixedHex(commitment), { value: SEND_VALUE, from: SENDER });
-    //     globalTree.insert(commitment);
-    //     verifyMerklePath(globalTree, TREE_LEVELS, commitment, logs[0]);
-  
-    //     await globalAssetPool.deposit(toFixedHex(commitment), { value: SEND_VALUE, from: SENDER })
-    //           .should.be.rejectedWith('The commitment has been submitted');
-    // })
-
-    // it('Should emit event once a commitment is proven', async () => {
-    //     let commitment = bigInt(42);
-    //     let { logs } = await globalAssetPool.deposit(toFixedHex(commitment), { value: SEND_VALUE, from: SENDER });
-    //     globalTree.insert(commitment);
-    //     verifyMerklePath(globalTree, TREE_LEVELS, commitment, logs[0]);
-    //     // logs[0].event.should.be.equal('Deposit');
-    //     // logs[0].args.commitment.should.be.equal(commitment);
-    //     // logs[0].args.leafIndex.should.be.eq.BN(0);
-
-    //     commitment = bigInt(12);
-    //     ;({ logs } = await globalAssetPool.deposit(toFixedHex(commitment), {value: SEND_VALUE, from: accounts[2] }))
-    //     globalTree.insert(commitment);
-    //     verifyMerklePath(globalTree, TREE_LEVELS, commitment, logs[0]);
-
-    //     // logs[0].event.should.be.equal('Deposit');
-    //     // logs[0].args.commitment.should.be.equal(commitment);
-    //     // logs[0].args.leafIndex.should.be.eq.BN(1);
-    // })
-  })
 })
